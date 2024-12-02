1b8199c0a647f5befd599690c5b588b95cb485b8
neursh <neurs@spaark.click>
2024-12-02 21:25:38 +0700

    Fix reloading issue

--- src/components/DisplayGraph.tsx
+++ src/components/DisplayGraph.tsx
@@ -1,23 +1,23 @@
 import ForceSupervisor from 'graphology-layout-force/worker';
-import { forwardRef, MutableRefObject, useEffect, useRef } from 'react';
+import { forwardRef, MutableRefObject, useEffect } from 'react';
 import Sigma from 'sigma';
 import Context from '../context';
 
 const DisplayGraph = forwardRef<HTMLElement>((_, container) => {
   container = container as MutableRefObject<HTMLElement>;
-  const initialized = useRef(false);
 
   useEffect(() => {
-    if (container.current && !initialized.current) {
-      initialized.current = true;
+    let renderer: Sigma | null = null;
+    let layout: ForceSupervisor | null = null;
 
-      const layout = new ForceSupervisor(Context.graph, {
+    if (container.current) {
+      layout = new ForceSupervisor(Context.graph, {
         isNodeFixed: (id, attr) =>
           attr.highlighted || Context.tree.root?.id === id,
       });
       layout.start();
 
-      const renderer = new Sigma(Context.graph, container.current, {
+      renderer = new Sigma(Context.graph, container.current, {
         labelSize: 24,
       });
 
@@ -28,14 +28,14 @@ const DisplayGraph = forwardRef<HTMLElement>((_, container) => {
         isDragging = true;
         draggedNode = e.node;
         Context.graph.setNodeAttribute(draggedNode, 'highlighted', true);
-        if (!renderer.getCustomBBox())
-          renderer.setCustomBBox(renderer.getBBox());
+        if (!renderer!.getCustomBBox())
+          renderer!.setCustomBBox(renderer!.getBBox());
       });
 
       renderer.on('moveBody', ({ event }) => {
         if (!isDragging || !draggedNode) return;
 
-        const pos = renderer.viewportToGraph(event);
+        const pos = renderer!.viewportToGraph(event);
 
         Context.graph.setNodeAttribute(draggedNode, 'x', pos.x);
         Context.graph.setNodeAttribute(draggedNode, 'y', pos.y);
@@ -55,6 +55,15 @@ const DisplayGraph = forwardRef<HTMLElement>((_, container) => {
       renderer.on('upNode', handleUp);
       renderer.on('upStage', handleUp);
     }
+
+    return () => {
+      if (layout) {
+        layout.kill();
+      }
+      if (renderer) {
+        renderer.kill();
+      }
+    };
   }, [container]);
 
   return <section ref={container} className="w-full h-screen"></section>;
